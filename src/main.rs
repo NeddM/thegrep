89e1a533f47187a3c0d478c8ff3b0e5e6df86afe
Nedd Chairi <neddyjob@gmail.com>
2023-09-14 13:40:19 +0200

    🎉 added -A flag, testing -B flag

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,40 @@ fn main() {
 }
 
 fn grep_document(args: &Vec<String>) {
-    let mut n_line = 0;
+    let mut n_line: usize = 0;
     let document = fs::read_to_string(&args[1]).unwrap();
+    let mut document_by_lines: Vec<&str> = vec![];
 
     for line in document.lines() {
-        n_line = n_line + 1;
-        if line.contains(&args[2]) {
-            println!("{}", line);
-            // if args.contains(&"-A".to_string()) {
-            //     println!("{}", "hola")
-            // }
+        document_by_lines.push(line);
+    }
 
-            if let Some(index) = args.iter().position(|arg| arg == "-A") {
-                if let Ok(n) = args[index + 1].parse::<u8>() {
-                    println!("{}", n);
+    for line in &document_by_lines {
+        n_line = n_line + 1;
+
+        if line.contains(&args[2]) {
+            if let Some(index) = args.iter().position(|pos| pos == "-B") {
+                if let Ok(n) = args[index + 1].parse::<usize>() {
+                    for i in (1..n).rev() {
+                        let new_index = (n_line - i) - 1;
+                        if (n_line - i) == 0 {
+                            break;
+                        }
+                        println!("{}", new_index);
+                        println!("{}", document_by_lines[new_index]);
+                    }
+                }
+            }
+
+            println!("{}", line);
+
+            if let Some(index) = args.iter().position(|pos| pos == "-A") {
+                if let Ok(n) = args[index + 1].parse::<usize>() {
+                    for i in 0..n {
+                        if i + n_line < document_by_lines.len() {
+                            println!("{}", document_by_lines[i + n_line])
+                        }
+                    }
                 }
             }
         }
